4a01d5b578459c626cc3bc0f9272fb3e64a10c64
qa598 <farrukhburiev001@gmail.com>
2023-03-05 16:50:13 +0500

    Initial commit

---
+++ app/src/main/java/farrukh/shop_centre/MainActivity.kt
@@ -0,0 +1,11 @@
+package farrukh.shop_centre
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
